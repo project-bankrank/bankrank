d1b783fb5dc6833605e193494ec66942b1041529
Anthony Calderaro <ajc-sorin@protonmail.com>
2024-02-01 23:05:21 -0500

    Updates code comment in productScripts.ts

--- src/productScripts/productScripts.ts
+++ src/productScripts/productScripts.ts
@@ -1,26 +1,24 @@
 /* 
-This is the primary import/export file which is used to consolidate the various template imports into a single location
+  This is the primary import/export file which is used to consolidate
+  the various template scripts into a single location, so the scrapingEngine
+  only has a single import.
 
-Each directory within /templates is a financial institution such as a Bank or Credit Union. For instance, /templates/BankOfAmerica or /templates/WellsFargo
+  Each directory within /productScripts is a financial institution such
+  as a Bank or Credit Union, such as /productScripts/BankOfAmerica or
+  /productScripts/NavyFederal.
 
-Each of these institution-directories contains at least 1 file. Each file represents a template for scraping a specific product and tier. Most instituions have at least 2 products (checking and savings accounts), but many also have money market accounts and Time Deposit. Each of these products are usually offered at multiple tiers, to incentivze customers to put more of their funds into the bank. As a result, it is common to find a single bank with dozens of product-tier offerings.
+  Each of these institution's directory contains a file for each product
+  offering (savings, checking, etc). The file for each product offering 
+  contians at least 1 function which is a playwright script for
+  scraping a specified tier of the product. Some banks offer multiple 
+  tiers of a product offering, so it is Bank Rank's objective to monitor
+  every tier, for every product.
 
-For instance, a bank may have the following 4 products: Savings, Checking, MMA, CD. Each product could have 3 tiers: $0-$1,000; $1,000-$100,000; $100,000+, for a total of 12 distinct offerings, each requiring their own file. To make organizing imports/exports more efficient, each institution has an `index.js` file which acts as the consolidator of imports and provides a single export to the template engine. As a result, the file structure for this bank could look like this:
-  - /templates/exampleBank/index.js
-  - /templates/exampleBank/checkingTier1.js
-  - /templates/exampleBank/checkingTier2.js
-  - /templates/exampleBank/checkingTier3.js
-  - /templates/exampleBank/SavingsTier1.js
-  - /templates/exampleBank/SavingsTier2.js
-  - /templates/exampleBank/SavingsTier3.js
-  - /templates/exampleBank/mmaTier1.js
-  - /templates/exampleBank/mmaTier2.js
-  - /templates/exampleBank/mmaTier3.js
-  - /templates/exampleBank/cdTier1.js
-  - /templates/exampleBank/cdTier2.js
-  - /templates/exampleBank/cdTier3.js
+  All product offerings are imported into an index.ts file in each institution
+  directory (productScripts/BankOfAmerica/index.ts). That index is imported at
+  the top of this file, where its contents are spread out using the Object.values
+  pattern below
 */
-
 
 import americaFirst from './AmericaFirst/index.js';
 import bankOfAmericaSavings from './BankOfAmerica/index.js';
